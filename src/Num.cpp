6ed8316ac34fed1723473b30106417ed17e96bc3
Keith Lim <Keith@Keiths-MacBook-Pro.local>
2018-02-13 12:16:23 -0500

    added examples

---
+++ src/Num.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Num.hpp"
+void sayHello();
+
+void sayHello()//Implementation
+{
+    std::cout << "Hello!";
+};
---
+++ src/Num.hpp
@@ -0,0 +1,16 @@
+//
+//  Num.hpp
+//  map
+//
+//  Created by Keith Lim on 2/12/18.
+//  Copyright © 2018 Keith Lim. All rights reserved.
+//
+
+#ifndef Num_hpp
+#define Num_hpp
+
+#include <stdio.h>
+
+void sayHello();
+
+#endif /* Num_hpp */
