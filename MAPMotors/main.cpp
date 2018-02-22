090429889b4170d9467964c3c4759eeefa30f6c9
Keith lim <keithlimweiluen@gmail.com>
2018-02-22 11:11:57 -0500

    Initial Commit

---
+++ MAPMotors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MAPMotors
+//
+//  Created by Keith Lim on 2/22/18.
+//  Copyright © 2018 MAP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
